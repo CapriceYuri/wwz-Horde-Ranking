c88b053793848b6a8babdd6e6f41191d6182c63a
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-10-25 21:28:39 -0400

    Initialization of WWZ #2 verison

---
+++ src/script.js
@@ -0,0 +1,36 @@
+"use strict";
+const summary = document.querySelector("#btnradio0");
+const summaryBox = document.querySelector(".BOX0");
+const hardSelect = document.querySelector("#btnradio1");
+const hardBox = document.querySelector(".BOX1");
+const normalSelect = document.querySelector("#btnradio2");
+const normalBox = document.querySelector(".BOX2");
+// Content Tabs Logic
+summary.addEventListener('click', () => {
+    hardSelect.checked = false;
+    hardBox.style.display = 'none';
+    normalSelect.checked = false;
+    ;
+    normalBox.style.display = 'none';
+    summaryBox.style.display = 'block';
+});
+normalSelect.addEventListener('click', () => {
+    hardSelect.checked = false;
+    hardBox.style.display = 'none';
+    summary.checked = false;
+    summaryBox.style.display = 'none';
+    normalBox.style.display = 'block';
+});
+hardSelect.addEventListener('click', () => {
+    normalSelect.checked = false;
+    normalBox.style.display = 'none';
+    summary.checked = false;
+    summaryBox.style.display = 'none';
+    hardBox.style.display = 'block';
+});
+// Content Entry Logic
+class RecordInfo {
+    constructor(map, player, wave, src, team, medal) { }
+}
+const player1 = new RecordInfo('Tokyo', "CapriceYuri", 99, "http://www.youtube.com");
+console.log(player1);
