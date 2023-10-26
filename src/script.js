65651a10171dd943e4fa645abc550e96c2ad6f56
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-10-25 23:25:20 -0400

    Added function for input player run

--- src/script.js
+++ src/script.js
@@ -28,9 +28,90 @@ hardSelect.addEventListener('click', () => {
     summaryBox.style.display = 'none';
     hardBox.style.display = 'block';
 });
-// Content Entry Logic
-class RecordInfo {
-    constructor(map, player, wave, src, team, medal) { }
+// Hard/Normal Maps
+const japanH = document.querySelector(".h-japan");
+function addRecord(obj) {
+    const tempRow = document.createElement("tr");
+    tempRow.classList.add("text-center", "align-middle");
+    japanH.appendChild(tempRow);
+    const map = document.createElement("td");
+    let group = undefined;
+    let whitespace = undefined;
+    map.textContent = "";
+    tempRow.appendChild(map);
+    if (obj.medal !== undefined) {
+        if (obj.medal === "First") {
+            const medalGold = document.createElement("img");
+            medalGold.setAttribute("src", "images/firstPlace.png");
+            map.appendChild(medalGold);
+        }
+        if (obj.medal === "Second") {
+            const medalGold = document.createElement("img");
+            medalGold.setAttribute("src", "images/secondPlace.png");
+            map.appendChild(medalGold);
+        }
+        if (obj.medal === "Third") {
+            const medalGold = document.createElement("img");
+            medalGold.setAttribute("src", "images/thirdPlace.png");
+            map.appendChild(medalGold);
+        }
+    }
+    if (obj.team === "Solo") {
+        group = document.createElement("i");
+        whitespace = document.createElement("span");
+        whitespace.textContent = " ";
+        map.appendChild(whitespace);
+        group.classList.add('fa-solid', 'fa-person-rifle', 'text-danger');
+        map.appendChild(group);
+    }
+    else {
+        group = document.createElement("i");
+        whitespace = document.createElement("span");
+        whitespace.textContent = " ";
+        map.appendChild(whitespace);
+        group.classList.add('fa-solid', 'fa-people-group', 'text-success');
+        map.appendChild(group);
+    }
+    const player = document.createElement("td");
+    player.textContent = obj.player;
+    tempRow.appendChild(player);
+    const waves = document.createElement("td");
+    waves.textContent = obj.wave;
+    tempRow.appendChild(waves);
+    const src = document.createElement("td");
+    const link = document.createElement("a");
+    link.textContent = 'Full';
+    link.href = obj.src;
+    link.target = "_blank";
+    src.appendChild(link);
+    tempRow.appendChild(src);
 }
-const player1 = new RecordInfo('Tokyo', "CapriceYuri", 99, "http://www.youtube.com");
-console.log(player1);
+let obj1 = {
+    player: 'Cap',
+    wave: '99',
+    src: 'http://www.youtube.com',
+    medal: "First"
+};
+let userData = [
+    {
+        player: 'PlayerOne',
+        wave: '99',
+        src: 'http://www.youtube.com',
+        medal: "First"
+    },
+    {
+        player: 'PlayerTwo',
+        wave: '23',
+        src: 'http://www.youtube.com',
+        medal: "Second"
+    },
+    {
+        player: 'PlayerThree',
+        wave: '50',
+        src: 'http://www.youtube.com',
+        medal: "Third"
+    },
+];
+for (let i = 0; i < userData.length; i++) {
+    addRecord(userData[i]);
+}
--- src/script.ts
+++ src/script.ts
@@ -32,16 +32,101 @@ hardSelect.addEventListener('click', () => {
 
 // Content Entry Logic
 
-class RecordInfo {
-    constructor(
-        map: string,
-        player: string,
-        wave: number,
-        src: string,
-        team?: string,
-        medal?: string
-    ) { }
+interface RecordInfo {
+    player: string,
+    wave: string,
+    src: string,
+    team?: string,
+    medal?: string
 }
-const player1 = new RecordInfo('Tokyo', "CapriceYuri", 99, "http://www.youtube.com")
 
-console.log(player1)
+// Hard/Normal Maps
+
+const japanH = document.querySelector(".h-japan") as HTMLTableSectionElement;
+
+function addRecord(obj: RecordInfo): void {
+    const tempRow = document.createElement("tr") as HTMLTableRowElement;
+    tempRow.classList.add("text-center", "align-middle")
+    japanH.appendChild(tempRow);
+    const map = document.createElement("td");
+    let group = undefined;
+    let whitespace = undefined;
+    map.textContent = "";
+    tempRow.appendChild(map);
+    if (obj.medal !== undefined) {
+        if (obj.medal === "First") {
+            const medalGold = document.createElement("img");
+            medalGold.setAttribute("src", "images/firstPlace.png");
+            map.appendChild(medalGold)
+        }
+        if (obj.medal === "Second") {
+            const medalGold = document.createElement("img");
+            medalGold.setAttribute("src", "images/secondPlace.png");
+            map.appendChild(medalGold)
+        }
+        if (obj.medal === "Third") {
+            const medalGold = document.createElement("img");
+            medalGold.setAttribute("src", "images/thirdPlace.png");
+            map.appendChild(medalGold)
+        }
+    }
+    if (obj.team === "Solo") {
+        group = document.createElement("i");
+        whitespace = document.createElement("span")
+        whitespace.textContent = " "
+        map.appendChild(whitespace)
+        group.classList.add('fa-solid', 'fa-person-rifle', 'text-danger');
+        map.appendChild(group);
+    } else {
+        group = document.createElement("i");
+        whitespace = document.createElement("span")
+        whitespace.textContent = " "
+        map.appendChild(whitespace)
+        group.classList.add('fa-solid', 'fa-people-group', 'text-success');
+        map.appendChild(group);
+    }
+    const player = document.createElement("td");
+    player.textContent = obj.player;
+    tempRow.appendChild(player);
+    const waves = document.createElement("td");
+    waves.textContent = obj.wave;
+    tempRow.appendChild(waves);
+    const src = document.createElement("td");
+    const link = document.createElement("a")
+    link.textContent = 'Full'
+    link.href = obj.src;
+    link.target = "_blank"
+    src.appendChild(link)
+    tempRow.appendChild(src);
+}
+
+let obj1 = {
+    player: 'Cap',
+    wave: '99',
+    src: 'http://www.youtube.com',
+    medal: "First"
+}
+let userData = [
+    {
+        player: 'PlayerOne',
+        wave: '99',
+        src: 'http://www.youtube.com',
+        medal: "First"
+    },
+    {
+        player: 'PlayerTwo',
+        wave: '23',
+        src: 'http://www.youtube.com',
+        medal: "Second"
+    },
+    {
+        player: 'PlayerThree',
+        wave: '50',
+        src: 'http://www.youtube.com',
+        medal: "Third"
+    },
+]
+
+for (let i = 0; i < userData.length; i++) {
+    addRecord(userData[i]);
+}
