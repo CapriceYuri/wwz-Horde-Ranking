d85219c622a34612ec534ba5bef5404a2289d82c
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-11-02 14:50:40 -0400

    Finalized icns

--- src/script.ts
+++ src/script.ts
@@ -349,10 +349,11 @@ console.log(formattedToday);
 console.log(data)
 
 const saberprank = document.createElement("img");
-saberprank.setAttribute("src", "images/reaper.png")
+saberprank.setAttribute("src", "images/saber.png")
 const saberprankName = document.createElement("span")
-saberprankName.textContent = "  Saber Interactive"
+saberprankName.textContent = " Saber Interactive"
 saberprankName.classList.add("text-warning")
+
 nyH.childNodes[5].childNodes[1].appendChild(saberprank);
 nyH.childNodes[5].childNodes[1].appendChild(saberprankName);
 
