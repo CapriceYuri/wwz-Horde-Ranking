a4fee621c73c795639cf0b36e83a87fe34a156d0
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-11-29 13:42:13 -0500

    Testing TS pathing

--- src/script.ts
+++ src/script.ts
@@ -426,7 +426,7 @@ function addGalleyImage(obj: ImgRecord): void {
     tempColCard.classList.add('card', 'text-bg-dark');
     const imgContent = document.createElement('img');
     imgContent.classList.add('card-img', 'card-fluid');
-    imgContent.setAttribute('src', `./images/${obj.imgUrl}.png`);
+    imgContent.setAttribute('src', 'images/img1.png');
     const imgContentAuthor = document.createElement('div');
     imgContentAuthor.classList.add('card-img-overlay');
     imgContentAuthor.innerText = `@ ${obj.imgAuthor}`
